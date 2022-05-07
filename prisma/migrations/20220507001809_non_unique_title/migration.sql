b2ba5e3edd0aa5f0b8cd56fc4f80fed094a72b84
Nfinger <nfinger1020@gmail.com>
2022-05-06 20:22:33 -0400

    add db seed script

---
+++ prisma/migrations/20220507001809_non_unique_title/migration.sql
@@ -0,0 +1,2 @@
+-- DropIndex
+DROP INDEX "Progress_title_key";
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,7 +4,44 @@ import bcrypt from "bcryptjs";
 const prisma = new PrismaClient();
 
 async function seed() {
-  console.log(`Database has been seeded. 🌱`);
+  await prisma.progress.deleteMany({ where: {} });
+  await prisma.location.deleteMany({ where: {} });
+
+  await prisma.progress.create({
+    data: {
+      title: "The Hunt",
+      currentStep: 0,
+    },
+  });
+
+  await prisma.location.createMany({
+    data: [
+      {
+        name: "Shojo",
+        order: 0,
+        hasBeenGuessed: false,
+        hasBeenVisited: false,
+        clue: "Come visit my HOOD, my graffitied walls express my individuality, I stand out from my surroundings while paying homage to my Asian roots. I even make tofu delicious with the help of some fries and Brian Moy",
+        boldWord: "HOOD",
+      },
+      {
+        name: "Yvonnes",
+        order: 1,
+        hasBeenGuessed: false,
+        hasBeenVisited: false,
+        clue: "While I make CLAIMS of being a beauty salon, my not so secret door traps the scent of espresso and vodka in a dimly lit room home to prestigious generals and dazzling chandeliers",
+        boldWord: "CLAIMS",
+      },
+      {
+        name: "Contessa",
+        order: 2,
+        hasBeenGuessed: false,
+        hasBeenVisited: false,
+        clue: "I am a Countess not a COUNT, I speak only Italian, enjoy a nice view of a sunset, and  rubbing elbows with people headed to fancy balls.",
+        boldWord: "COUNT",
+      },
+    ],
+  });
 }
 
 seed()
